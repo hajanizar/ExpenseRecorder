33b38d448781b8fef737a67015800646f493f998
hajanizar <haja.nizar@hispark.in>
2014-02-07 16:27:40 +0530

    Expense Recorder
    Modulated Program

---
+++ ExpenseRecorder/AddExpenseRecorderViewController.h
@@ -0,0 +1,17 @@
+//
+//  AddExpenseRecorderViewController.h
+//  ExpenseRecorder
+//
+//  Created by BSA Univ4 on 05/02/14.
+//  Copyright (c) 2014 c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AddExpenseRecorderViewController : UITableViewController<UITextFieldDelegate>
+{
+    
+}
+
+@end
